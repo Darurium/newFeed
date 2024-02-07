e6ed162ae8c5150599423e348526420d65c5a443
KonshinAS <darurium@gmail.com>
2024-02-07 22:55:30 +0500

    2

---
+++ script.js
@@ -0,0 +1,377 @@
+const data = {
+    "sources": [
+        {
+            "id": 1,
+            "name": "meduza"
+        },
+        {
+            "id": 6,
+            "name": "3dnews"
+        },
+        {
+            "id": 7,
+            "name": "nytimes"
+        },
+        {
+            "id": 8,
+            "name": "forbes"
+        },
+        {
+            "id": 9,
+            "name": "igromania"
+        },
+        {
+            "id": 10,
+            "name": "buro237"
+        },
+        {
+            "id": 11,
+            "name": "rusvesna"
+        },
+        {
+            "id": 12,
+            "name": "7ya"
+        },
+        {
+            "id": 14,
+            "name": "aif"
+        },
+        {
+            "id": 15,
+            "name": "gazetaru"
+        },
+        {
+            "id": 16,
+            "name": "karpov.courses"
+        }
+    ],
+    "categories": [
+        {
+            "id": 1,
+            "name": "tech"
+        },
+        {
+            "id": 2,
+            "name": "sport"
+        },
+        {
+            "id": 3,
+            "name": "fashion"
+        },
+        {
+            "id": 4,
+            "name": "politics"
+        },
+        {
+            "id": 5,
+            "name": "other"
+        },
+        {
+            "id": 6,
+            "name": "karpov.courses"
+        }
+    ],
+    "items": [
+        {
+            "id": 500454,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:30:45 GMT+0300 (Moscow Standard Time)",
+            "title": "В Кемеровской области овчарка до смерти загрызла ребенка",
+            "description": "СК возбудил уголовное дело.",
+            "image": "https://aif-s3.aif.ru/images/035/745/c6ecf42b98d9ec945449b131ab1a3fc5.jpg",
+            "source_id": 14,
+            "category_id": 5
+        },
+        {
+            "id": 500474,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:30:11 GMT+0300 (Moscow Standard Time)",
+            "title": "Россия запросила срочное заседание в СБ ООН",
+            "description": "Первый заместитель постоянного представителя Российской Федерации при Организации Объединенных Наций (ООН) Дмитрий Полянский заявил, что российская сторона запросила проведение срочного заседания Совета Безопасности ООН в связи с ударом ...",
+            "image": "https://img.gazeta.ru/files3/423/18172423/upload-RIA_8603831-pic905-895x505-85179.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500524,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:29:39 GMT+0300 (Moscow Standard Time)",
+            "title": "Ученые назвали идеальное время для завтрака",
+            "description": "Ученые из Барселонского института глобального здравоохранения (ISGlobal) в Испании рассказали, что завтрак до восьми утра сокращает риск развития сахарного диабета на 59%. Результаты исследования опубликованы в International Journal of ...",
+            "image": "https://img.gazeta.ru/files3/630/14723630/kkdkd-pic905-895x505-50317.jpg",
+            "source_id": 15,
+            "category_id": 1
+        },
+        {
+            "id": 500460,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:29:07 GMT+0300 (Moscow Standard Time)",
+            "title": "РФ запросила срочное заседание СБ ООН из-за удара ВСУ по Лисичанску",
+            "description": "Заседание могут провести во вторник, 6 февраля.",
+            "image": "https://aif-s3.aif.ru/images/035/745/6357898318456fdf1bc6abb84843cf7e.jpg",
+            "source_id": 14,
+            "category_id": 5
+        },
+        {
+            "id": 500508,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:28:47 GMT+0300 (Moscow Standard Time)",
+            "title": "Экс-советник главы Пентагона высказался о последствиях изъятия российских активов для США",
+            "description": "В случае конфискации западными странами российских активов для передачи их Украине многие государства перестанут хранить свои резервы в США. Такое мнение в интервью журналисту Стивену Гарднеру выразил экс-советник главы Пентагона полковник ...",
+            "image": "https://img.gazeta.ru/files3/499/16377499/190911101036-douglas-macgregor-file-pic905-895x505-94811.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500455,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:26:18 GMT+0300 (Moscow Standard Time)",
+            "title": "Кооперативный экшен Helldivers 2 будет получать бесплатный контент в обновлениях",
+            "description": "",
+            "image": "https://newcdn.igromania.ru/articles/pics/tmp/images/2024/2/5/76b474e8-58e9-4ef6-b3fb-9b8d1e0f8928.jpg",
+            "source_id": 9,
+            "category_id": 1
+        },
+        {
+            "id": 500477,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:20:45 GMT+0300 (Moscow Standard Time)",
+            "title": "Трамп: за сенатский проект по Украине проголосует \"только дурак\"",
+            "description": "Бывший президент США Дональд Трамп заявил, что только \"дурак или радикальный левый демократ\" проголосует за \"чудовищный\" законопроект, представленный американским сенатом для помощи Украине и охраны американской ...",
+            "image": "https://img.gazeta.ru/files3/15/18100015/29-pic905-895x505-79060.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500466,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:20:12 GMT+0300 (Moscow Standard Time)",
+            "title": "В Москве начались съемки «Возвращения попугая Кеши» с Никитой Кологривым",
+            "description": "Работа над картиной продолжится в Сочи.",
+            "image": "https://aif-s3.aif.ru/images/035/745/57fd9c0eb3aa703b531e6894a85549bc.jpg",
+            "source_id": 14,
+            "category_id": 5
+        },
+        {
+            "id": 500479,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:19:41 GMT+0300 (Moscow Standard Time)",
+            "title": "Медведев спрогнозировал уменьшение Украины до Львовской области",
+            "description": "Заместитель председателя Совета безопасности РФ Дмитрий Медведев уверен, что в будущем украинская территория сократится до пределов Львовской области. Пост с соответствующим содержанием он опубликовал на своей англоязычной странице в ...",
+            "image": "https://img.gazeta.ru/files3/582/17219582/RIA_8459128-pic905-895x505-51091.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500453,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:19:00 GMT+0300 (Moscow Standard Time)",
+            "title": "Компания отдала мошенникам $25,6 млн после видеоконференции с дипфейками",
+            "description": "Транснациональная компания потеряла 200 млн гонконгских долларов ($26 млн) из-за мошенников, использовавших технологию дипфейка на новом уровне. Злоумышленникам удалось убедить сотрудника офиса компании в Гонконге перечислить указанную сумму с помощью фальшивого группового видеозвонка, в котором с помощью дипфейка участвовали поддельный финансовый директор и несколько сотрудников компании. \n#дипфейки #мошенничество #гонконг",
+            "image": "https://3dnews.ru/assets/external/illustrations/2024/02/05/1099843/hacker-2300772_1280.jpg",
+            "source_id": 6,
+            "category_id": 1
+        },
+        {
+            "id": 500525,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:15:15 GMT+0300 (Moscow Standard Time)",
+            "title": "Российские ученые провели пересадку напечатанной на 3D-принтере печени",
+            "description": "Ученые Института регенеративной медицины Первого МГМУ им. И.М. Сеченова пересадили лабораторной мыши фрагмент печени, напечатанный на 3D-принтере. Об этом Научно-технологический парк биомедицины университета сообщил изданию ...",
+            "image": "https://img.gazeta.ru/files3/765/17953765/70-pic905-895x505-81173.jpg",
+            "source_id": 15,
+            "category_id": 1
+        },
+        {
+            "id": 500470,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:14:54 GMT+0300 (Moscow Standard Time)",
+            "title": "Карантин по кори введен в областной детской больнице в Ростове-на-Дону",
+            "description": "Ограничения будут действовать до 22 февраля.",
+            "image": "https://aif-s3.aif.ru/images/035/745/3625f0a2a5e3de5d8c8fb02aef14e012.jpg",
+            "source_id": 14,
+            "category_id": 5
+        },
+        {
+            "id": 500472,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:13:51 GMT+0300 (Moscow Standard Time)",
+            "title": "Вступившего в РДК актера Канахина будут заочно судить в России за госизмену",
+            "description": "Актер из сериала «Глухарь» участвовал в нападении на Россию.",
+            "image": "https://aif-s3.aif.ru/images/035/745/a35ebff738d7f122ef870b67e00a6330.jpg",
+            "source_id": 14,
+            "category_id": 4
+        },
+        {
+            "id": 500526,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:10:45 GMT+0300 (Moscow Standard Time)",
+            "title": "Стало известно, что домашние кошки могут благотворно влиять на здоровье хозяев",
+            "description": "Ученые из университета Миннесоты в США обнаружили, что владельцы кошек в меньшей степени подвержены стрессам. Также у них снижен риск инфаркта и других заболеваний сердца и сосудов. Результаты исследования опубликованы в Journal of ...",
+            "image": "https://img.gazeta.ru/files3/176/15153176/herman-delgado-O4vTKtQo9BM-unsplash-pic905-895x505-50631.jpg",
+            "source_id": 15,
+            "category_id": 1
+        },
+        {
+            "id": 500475,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:03:53 GMT+0300 (Moscow Standard Time)",
+            "title": "ЦБ рассказал о новой схеме кражи аккаунтов в Telegram",
+            "description": "Мошенники предлагают защитить данные профиля.",
+            "image": "https://aif-s3.aif.ru/images/035/745/ed6d5d000403c606409ef0d62a9508dc.jpg",
+            "source_id": 14,
+            "category_id": 5
+        },
+        {
+            "id": 500478,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:03:18 GMT+0300 (Moscow Standard Time)",
+            "title": "На Украине заявили, что Залужный согласился стать послом в Великобритании",
+            "description": "По словам депутата Верховной Рады, главкома могут отправить в отставку в ближайшие дни.",
+            "image": "https://aif-s3.aif.ru/images/035/745/4567565d930c4b9751e2a845ecadade3.jpg",
+            "source_id": 14,
+            "category_id": 4
+        },
+        {
+            "id": 500482,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 18:00:47 GMT+0300 (Moscow Standard Time)",
+            "title": "В Верховной раде Украины рассказали о новом назначении Залужного",
+            "description": "Депутат Верховной рады Украины Евгений Шевченко утверждает, что главнокомандующий Вооруженными силами Украины (ВСУ) Валерий Залужный согласился уехать послом в Великобританию. Об этом сообщает ТАСС.\n\nДепутат подчеркнул, что Залужный скорее ...",
+            "image": "https://img.gazeta.ru/files3/821/18218821/2024-02-01T184114Z_1495825372_RC2055AXV5VE_RTRMADP_3_UKRAINE-CRISIS-ZALUZHNYI-pic905-895x505-48003.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500452,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 17:59:33 GMT+0300 (Moscow Standard Time)",
+            "title": "Правящая партия Венгрии бойкотировала голосование по вступлению Швеции в НАТО",
+            "description": "Правящая партия Венгрии «Фидес», которая имеет большинство мест в парламенте, бойкотировала голосование по вопросу вступления Швеции в НАТО. Ранее премьер Венгрии Виктор Орбан в телефонном разговоре с генсеком НАТО Йенсом Столтенбергом заявлял, что правительство призывает венгерский парламент ратифицировать членство Швеции в НАТО при первой возможности",
+            "image": "",
+            "source_id": 8,
+            "category_id": 5
+        },
+        {
+            "id": 500484,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 17:58:15 GMT+0300 (Moscow Standard Time)",
+            "title": "Генсек ООН осудил удар ВСУ по пекарне в Лисичанске",
+            "description": "Генеральный секретарь ООН Антониу Гутерриш осудил удар украинских войск по Лисичанску. Об этом пишет ТАСС со ссылкой на письмо, которое официальный представитель ООН Стефан Дюжаррик разослал журналистам.\n\n\"Генеральный секретарь ...",
+            "image": "https://img.gazeta.ru/files3/454/14770454/2022-04-22T171204Z_666675667_RC2F2T9G7A63_RTRMADP_3_UKRAINE-CRISIS-UN-GUTERRES-pic905-895x505-1269.jpg",
+            "source_id": 15,
+            "category_id": 4
+        },
+        {
+            "id": 500463,
+            "lang": "ru",
+            "date": "Mon Feb 05 2024 17:57:48 GMT+0300 (Moscow Standard Time)",
+            "title": "В студии авторов Baldurs Gate 3 высказались о переносе Starfield на PlayStation",
+            "description": "",
+            "image": "https://newcdn.igromania.ru/articles/pics/tmp/images/2024/2/5/71196271-d40b-4636-9bf6-a3b7798fdbd9.jpg",
+            "source_id": 9,
+            "category_id": 1
+        }
+    ]
+}
+
+
+const mainNews = data.items.slice(0, 3);
+const smallNews = data.items.slice(3, 12);
+
+const createMainNewsItem = (item) => {
+    const article = document.createElement("article");
+    const imgContainer = document.createElement("div");
+    const image = document.createElement("img");
+    const content = document.createElement("div");
+    const category = document.createElement("span");
+    const title = document.createElement("h2");
+    const text = document.createElement("p");
+    const source = document.createElement("span");
+
+    article.classList.add("main-article");
+    imgContainer.classList.add("main-article__img-container");
+    image.classList.add("main-article__img");
+    content.classList.add("main-article__content");
+    category.classList.add("main-article__category", "article__category");
+    title.classList.add("main-article__title");
+    text.classList.add("main-article__text");
+    source.classList.add("main-article__source", "article__source");
+
+    imgContainer.appendChild(image);
+    content.appendChild(category);
+    content.appendChild(title);
+    content.appendChild(text);
+    content.appendChild(source);
+
+    article.appendChild(imgContainer);
+    article.appendChild(content);
+
+    image.src = item.image;
+    category.textContent = data.categories.find(categoryItem => categoryItem.id === item.category_id).name; 
+    title.textContent = item.title;
+    text.textContent = item.description;
+    source.textContent = data.sources.find(sourcesItem => sourcesItem.id === item.source_id).name;
+
+    return article;
+}
+
+
+const createSmallNewsItem = (item) => {
+    const article = document.createElement("article");
+    const title = document.createElement("h2");
+    const caption = document.createElement("div");
+    const date = document.createElement("span");
+    const source = document.createElement("span");
+
+    article.classList.add("small-article");
+    title.classList.add("small-article__title");
+    caption.classList.add("small-article__caption");
+    date.classList.add("small-article__date", "article__date");
+    source.classList.add("small-article__source", "article__source");
+
+    caption.appendChild(date);
+    caption.append(source);
+    article.appendChild(title);
+    article.appendChild(caption)
+
+    const dateNews = new Date(item.date).toLocaleString("ru-RU", {month: "long", day: "2-digit"})
+
+    title.textContent = item.title;
+    source.textContent = data.sources.find(sourcesItem => sourcesItem.id === item.source_id).name;
+    date.textContent = dateNews;
+
+    return article;
+}
+
+const mainNewsContainer = document.querySelector(".articles__big-column");
+const smallNewsContainer = document.querySelector(".articles__small-column");
+
+mainNews.forEach(item => {
+    mainNewsContainer.appendChild(createMainNewsItem(item));
+})
+
+smallNews.forEach(item => {    
+    smallNewsContainer.appendChild(createSmallNewsItem(item));
+})
+
+
+{/* <article class="main-article">
+    <div class="main-article__img-container">
+        <img src="./images/image1.jpg" alt="Фото новости" class="main-article__img">
+    </div>
+    <div class="main-article__content">
+        <span class="main-article__category article__category">Технологии</span>
+        <h2 class="main-article__title">Отец жанра. Как уже забытый трип-хоп определяет самую популяр…. К</h2>
+        <p class="main-article__text">Новая мода на топовые наряды необычных цветов. В сезоне – топики, шорты-боксеры, сланцы и сандалии. А также большие солнечные очки и яркая шляпка.</p>
+        <span class="main-article__source article__source">Источник</span>
+    </div>
+</article> */}
+
+{/* <article class="small-article">
+    <h2 class="small-article__title">В американском зоопарке празднуют рождение гориллы: видео</h2>
+    <div class="small-article__caption">
+        <span class="small-article__date article__date">12 Июля</span>
+        <span class="small-article__source article__source">Источник</span>
+    </div>
+</article> */}
